ca2d2b0bfa9442390aad4cbd5a7e2d056dc792e0
ayemitibo <ayemitboabiodun02@gmail.com>
2019-07-31 11:36:49 +0100

    Chore : added bootstrap cards

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,26 +1,26 @@
-// import { mount } from '@vue/test-utils'
-// import indexPage from '@/pages/index.vue'
-// // import { actions } from '@/store/getphotos.js'
+import { mount } from '@vue/test-utils'
+import indexPage from '@/pages/index.vue'
+// import { actions } from '@/store/getphotos.js'
 
-// let url = ''
-// let data = ''
-// const mockUp = {
-//   get: (_url, _data) => {
-//     return new Promise((resolve, reject) => {
-//       url = _url
-//       data = _data
-//       resolve()
-//     })
-//   }
-// }
+let url = ''
+let data = ''
+const mockUp = {
+  get: (_url, _data) => {
+    return new Promise((resolve, reject) => {
+      url = _url
+      data = _data
+      resolve()
+    })
+  }
+}
 
-// describe('indexpage', () => {
-//   it('takes a list of images from the api', async () => {
-//     let payload = mount(indexPage,{
-//       mocks: {
-//         $http : mockUp
-//       }
-//     })
-//     expect(payload.vm.imageUrl).toBe([])
-//   })
-// })
+describe('indexpage', () => {
+  it('takes a list of images from the api', async () => {
+    let payload = mount(indexPage,{
+      mocks: {
+        $http : mockUp
+      }
+    })
+    expect(payload.vm.imageUrl).toBe([])
+  })
+})
